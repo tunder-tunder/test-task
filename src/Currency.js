266f675feffff997df48ae802f98b403cbc0f406
Alina Shcherbinina <alinkash@gmail.com>
2021-11-25 12:16:31 +0800

    tried

--- src/Currency.js
+++ src/Currency.js
@@ -3,7 +3,9 @@ import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators } from "./store/index";
 import { bindActionCreators } from "redux";
-
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 
 export const Currency = () => {
@@ -28,45 +30,110 @@ export const Currency = () => {
 };
 
 const Graph = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    let url = "https://freecurrencyapi.net/api/v2/historical?apikey=7eb0be50-4cf6-11ec-927d-f1b73c6174cc&base_currency=USD&date_from=2020-10-01&date_to=2021-11-24"
 
-    const currency = useSelector((state) => state.currency);
 
-    switch (currency) {
-        case "dollar":
-            return (
-                <><div>
-                    <p> {currency} </p>
-                </div><div>
-                        <p> graph goes here</p>
-                    </div>
-                </>
-            );
-        case "ruble":
-            return (
-                <><div>
-                    <p> {currency} </p>
-                </div><div>
-                        <p> graph goes here</p>
-                    </div>
-                </>
-            );
-        case "euro":
-            return (
-                <><div>
-                    <p> {currency} </p>
-                </div><div>
-                        <p> graph goes here</p>
-                    </div>
-                </>
-            );
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const asyncResponse = await axios(url);
+                const value = asyncResponse.data.data;
 
-        default:
+                setItems(value);
+                setIsLoaded(true);
+            } catch (err) {
+                setError(err);
+                setIsLoaded(false);
+                // console.error(err);
+            }
+        }
+
+        fetchData();
+
+    }, [items]);
+
+    console.log(items);
+   
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        if (items.length > 0) {
+            console.log(items);
             return (
-                <div>
-                    <p> choose something </p>
-                </div>
+                <ul>
+                    {items.map((data, key) => {
+                        return (
+                            <div key={key}>
+                                <TestTable key={key}
+                                    company={data.usd} />
+                            </div>
+                        );
+                    })}
+                </ul>
             );
+        } else return (
+            <p>Not yet returned</p>
+        )
     }
 
-    
+    // const currency = useSelector((state) => state.currency);
+
+    // switch (currency) {
+    //     case "dollar":
+    //         return (
+    //             <><div>
+    //                 <p> {currency} </p>
+    //             </div><div>
+    //                     <p> graph goes here</p>
+    //                 </div>
+    //             </>
+    //         );
+    //     case "ruble":
+    //         return (
+    //             <><div>
+    //                 <p> {currency} </p>
+    //             </div><div>
+    //                     <p> graph goes here</p>
+    //                 </div>
+    //             </>
+    //         );
+    //     case "euro":
+    //         return (
+    //             <><div>
+    //                 <p> {currency} </p>
+    //             </div><div>
+    //                     <p> graph goes here</p>
+    //                 </div>
+    //             </>
+    //         );
+
+    //     default:
+    //         return (
+    //             <div>
+    //                 <p> choose something </p>
+    //             </div>
+    //         );
+    // }
+
+
+};
+
+const TestTable = ({ company }) => {
+    if (!company) return <div />;
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <h5>{company}</h5>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
 };
